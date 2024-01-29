bd6473bf069ef58fc93e6a4092b0cf5108ad13eb
ASPS <lwh.asps@gmail.com>
2024-01-29 23:33:22 +0800

    update reduceinterger.cu kelnel 5

--- reduceInteger.cu
+++ reduceInteger.cu
@@ -131,6 +131,56 @@ __global__ void reduceUnrolling2(int *g_idata, int *g_odata, unsigned int n){
         g_odata[blockIdx.x] = idata[0];
 }
 
+__global__ void reduceCompleteUnrollWarps8(int *g_idata, int *g_odata, unsigned int n){
+    //set thread ID
+    unsigned int tid = threadIdx.x;
+    unsigned int idx = blockIdx.x*blockDim.x*8 + threadIdx.x;
+
+    //convert global data pointer to the local pointer of this block
+    int *idata = g_idata + blockIdx.x*blockDim.x*8;
+
+    //unrolling 2
+    if(idx + 7*blockDim.x < n){
+        int a1 = g_idata[idx];
+        int a2 = g_idata[idx + blockDim.x];
+        int a3 = g_idata[idx + 2*blockDim.x];
+        int a4 = g_idata[idx + 3*blockDim.x];
+        int b1 = g_idata[idx + 4*blockDim.x];
+        int b2 = g_idata[idx + 5*blockDim.x];
+        int b3 = g_idata[idx + 6*blockDim.x];
+        int b4 = g_idata[idx + 7*blockDim.x];
+        g_idata[idx] = a1 + a2 + a3 + a4 + b1 + b2 + b3 + b4;
+    }
+    __syncthreads();
+
+    //in-place reduction and complete unroll
+    if(blockDim.x >= 1024 && tid < 512) idata[tid] += idata[tid + 512];
+    __syncthreads();
+    if(blockDim.x >= 512 && tid < 256) idata[tid] += idata[tid + 256];
+    __syncthreads();
+    if(blockDim.x >= 256 && tid < 128) idata[tid] += idata[tid + 128];
+    __syncthreads();
+    if(blockDim.x >= 128 && tid < 64) idata[tid] += idata[tid + 64];
+    __syncthreads();
+
+
+
+    //unrolling warp
+    if(tid < 32){
+        volatile int *vmem = idata;
+        vmem[tid] += vmem[tid + 32];
+        vmem[tid] += vmem[tid + 16];
+        vmem[tid] += vmem[tid + 8];
+        vmem[tid] += vmem[tid + 4];
+        vmem[tid] += vmem[tid + 2];
+        vmem[tid] += vmem[tid + 1];
+    }
+
+    //write result for this block to global mem
+    if(tid == 0)
+        g_odata[blockIdx.x] = idata[0];
+}
+
 
 int main(int argc, char *argv[]) {
     //set up device
@@ -259,7 +309,21 @@ int main(int argc, char *argv[]) {
         gpu_sum += h_odata[i];
     printf("GPU reduceUnrolling2 elapsed %d ms gpu_sum: %d <<<grid %d block %d>>> \n", iElaps, gpu_sum, grid.x/2, block.x);
     cudaDeviceSynchronize();
-    
+
+    //kernel 5: reduceCompleteUnrollWarps8
+    cudaMemcpy(d_idata, h_idata, bytes, cudaMemcpyHostToDevice);
+    cudaDeviceSynchronize();
+    iStart = cpuSecond();
+    reduceCompleteUnrollWarps8<<<grid.x/2, block>>>(d_idata, d_odata, size);
+    cudaDeviceSynchronize();
+    iElaps = cpuSecond() - iStart;
+    cudaMemcpy(h_odata, d_odata, grid.x/2*sizeof(int), cudaMemcpyDeviceToHost);
+    gpu_sum = 0;
+    for(int i = 0; i < grid.x/2; ++i) 
+        gpu_sum += h_odata[i];
+    printf("GPU reduceCompleteUnrollWarps8 elapsed %d ms gpu_sum: %d <<<grid %d block %d>>> \n", iElaps, gpu_sum, grid.x/2, block.x);
+    cudaDeviceSynchronize();
+
 
     // gpu_sum = 0;
     // for(int i = 0; i < grid.x/8; ++i) 
